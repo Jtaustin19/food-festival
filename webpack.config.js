ec5a288f167e81f74ef476c8f4a3e9c09375aa08
Josh <jtaustin19@icloud.com>
2021-04-22 20:25:14 -0500

    added install, activate, and fetch functions to service-worker

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,33 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const path = require("path");
-const webpack = require("webpack");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        app: "./assets/js/script.js",
-        events: "./assets/js/events.js",
-        schedule: "./assets/js/schedule.js",
-        tickets: "./assets/js/tickets.js"
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
     },
+
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
     },
+
     module: {
         rules: [
             {
-                test: /\.jpg$/i,
+                test: /\.(jpe?g|png|gif)$/i,
                 use: [
                     {
                         loader: 'file-loader',
                         options: {
                             name (file) {
-                                return "[path][name].[ext]"
+                                return '[path][name].[ext]'
                             },
                             publicPath: function(url) {
-                                return url.replace("../", "/assets/")
+                                return url.replace('../', '/assets/')
                             }
                         }
                     },
@@ -36,14 +38,16 @@ module.exports = {
             }
         ]
     },
+
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
         new BundleAnalyzerPlugin({
-            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+            analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
         })
     ],
+
     mode: 'development'
 };
